ee397bc1b8054de2effeebbcc1a11f85aac0f41f
Timofey <teamheightline@mail.ru>
2020-09-08 12:14:13 -0700

    Первая команда пользователя теперь отрабатывается корректно

--- blueprints/testcomand.py
+++ blueprints/testcomand.py
@@ -10,8 +10,7 @@ from utils.constants import MENU_KB
 test_router = DefaultRouter()
 
 
-
-@simple_bot_message_handler(test_router, TextFilter("222"))
+@simple_bot_message_handler(test_router, TextFilter(text="начать", ignore_case=True))
 async def first_message_to_bot(event: SimpleBotEvent):
     return await event.answer(
         message="Добро пожаловаь, здесь будет собрана вся важная информация и расписание занятий",
