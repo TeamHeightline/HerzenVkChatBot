829f90d27e1d6a54fc79307eccea463d80fe7515
Timofey <teamheightline@mail.ru>
2020-09-17 23:36:49 +0300

    как бы первый json от Шафира! Понятно как теперь все организовать

--- blueprints/api.py
+++ blueprints/api.py
@@ -1,7 +1,10 @@
-
 import requests
 
-payload = {'faculty': 'институт информационных технологий и технологического образования', 'level': "бакалавриат"}
+# payload = {'faculty': 'институт информационных технологий и технологического образования', 'level': "бакалавриат",
+#            "type":"очная форма обучения"}
 
-r = requests.get('https://herzen-timetable.herokuapp.com//api/timetable/programs', params=payload)
+# r = requests.get('https://herzen-timetable.herokuapp.com/api/timetable/programs', params=payload)
+# print(r.text)
+payload = {"groupURL": "/static/schedule_view.php?id_group=12459&sem=1"}
+r = requests.get('https://herzen-timetable.herokuapp.com/api/timetable/group', payload)
 print(r.text)
