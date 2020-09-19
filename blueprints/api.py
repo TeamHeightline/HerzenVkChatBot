2f79b5a279f7ae5d38c0fd23d6cebbb500881252
Timofey <teamheightline@mail.ru>
2020-09-19 22:12:37 +0300

    Собрано в один метод))))

--- blueprints/api.py
+++ blueprints/api.py
@@ -1,38 +1,27 @@
 import requests
 import json
 
-payload = {"groupURL": "/static/schedule_view.php?id_group=12459&sem=1"}
-r = requests.get('https://herzen-timetable.herokuapp.com/api/timetable/group', payload)
 
-res_dict = json.loads(r.content)
-for i in range(0, 6):
-    print((res_dict['days'][i]['day']).title() + ' ' +
-          res_dict['days'][i]['hours'][0]['weeks'][0]['classes'][0]['dates'][0]['dates_raw'])
-    for a in range(0, 4):
-        print(res_dict['days'][i]['hours'][a]['timespan'])
-        try:
-            print(res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['class'] + " " +
-                  res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['type'] + " " +
-                  res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['teacher'] + " " +
-                  res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['place'] + "\n")
-        except:
-            pass
-    print("\n")
+async def get_timetable(url):
+    payload = {"groupURL": "/static/schedule_view.php?id_group=12459&sem=1"}
+    r = requests.get('https://herzen-timetable.herokuapp.com/api/timetable/group', payload)
 
-timetable_text = ''
+    res_dict = json.loads(r.content)
 
-for i in range(0, 6):
-    timetable_text += ((res_dict['days'][i]['day']).title() + ' ' +
-                       res_dict['days'][i]['hours'][0]['weeks'][0]['classes'][0]['dates'][0]['dates_raw'])
-    for a in range(0, 4):
-        timetable_text += (res_dict['days'][i]['hours'][a]['timespan'] + " ")
-        try:
-            timetable_text += (res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['class'] + " " +
-                               res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['type'] + " " +
-                               res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['teacher'] + " " +
-                               res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['place'] + "\n")
-        except:
-            pass
-    timetable_text += "\n"
+    timetable_text = ''
 
-print(timetable_text)
+    for i in range(0, 6):
+        timetable_text += ((res_dict['days'][i]['day']).title() + ' ' +
+                           res_dict['days'][i]['hours'][0]['weeks'][0]['classes'][0]['dates'][0]['dates_raw'] + "\n")
+        for a in range(0, 4):
+            timetable_text += (res_dict['days'][i]['hours'][a]['timespan'] + " ")
+            try:
+                timetable_text += (res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['class'] + " " +
+                                   res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['type'] + " " +
+                                   res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['teacher'] + " " +
+                                   res_dict['days'][i]['hours'][a]['weeks'][0]['classes'][0]['place'] + "\n")
+            except:
+                pass
+        timetable_text += " \n \n"
+
+    return(timetable_text)
